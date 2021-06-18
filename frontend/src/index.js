faa48d60c06143ba54ac17e60564b6c7472ad0cf
wilson <whuang3015@gmail.com>
2021-06-17 20:13:07 -0700

    test custom csrf cookie restoration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,14 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store/index'
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
