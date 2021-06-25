c277f37320dc21a11d44d0db1a66a7ca9a22f553
wilson <whuang3015@gmail.com>
2021-06-24 21:25:16 -0700

    finish up events api routes

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
+const router = require("express").Router();
+const asyncHandler = require("express-async-handler");
 const { check } = require("express-validator");
 const { requireAuth } = require("../../utils/auth");
-const { Event, User, Location, Group } = require('../../db/models');
+const { Event, User, Location, Group } = require("../../db/models");
 const { handleValidationErrors } = require("../../utils/validation");
 
 const validateEvent = [
@@ -12,7 +12,7 @@ const validateEvent = [
     .withMessage("Please provide a valid event name."),
   check("name")
     .isLength({ min: 3, max: 64 })
-    .withMessage("Please provide a group name between 3 and 64 characters."),
+    .withMessage("Please provide an event name between 3 and 64 characters."),
   check("date")
     .exists({ checkFalsy: true })
     .withMessage("Please provide a date for this event."),
@@ -24,21 +24,21 @@ const validateEvent = [
     .withMessage("Please provide a valid date format."),
   check("capacity")
     .exists({ checkFalsy: true })
-    .withMessage(
-      "Please provide a number of guests."),
+    .withMessage("Please provide a number of guests."),
   check("capacity")
     .isInt({ min: 0, max: 9999 })
-    .withMessage(
-      "Please provide a number of guests between 0 and 9999."),
+    .withMessage("Please provide a number of guests between 0 and 9999."),
   check("locationId")
     .exists({ checkFalsy: true })
     .withMessage("Please select a location for your event."),
   handleValidationErrors,
 ];
 
+/////// Event routes
+
 // GET / ---> Get all events
 router.get(
-  '/',
+  "/",
   asyncHandler(async (req, res, next) => {
     const { limit } = req.body;
     const events = await Event.findAll({
@@ -47,51 +47,50 @@ router.get(
         { model: Location, attributes: ["name", "locale"] },
         { model: Group, attributes: ["name"] },
       ],
-      order: [
-        ['date', 'DESC']
-      ],
+      order: [["date", "DESC"]],
       limit,
-    })
+    });
     res.json(events);
-  }))
+  })
+);
 
 // GET /:id ---> Get one specific event
 router.get(
-  '/:id',
+  "/:id",
   asyncHandler(async (req, res, next) => {
     const { id } = req.params;
     const events = await Event.findAll({
       include: [
-      { model: User, attributes: "username" },
-      { model: Location, attributes: ["name", "locale"] },
-      { model: Group, attributes: ["name"] },
-    ]
+        { model: User, attributes: "username" },
+        { model: Location, attributes: ["name", "locale"] },
+        { model: Group, attributes: ["name"] },
+      ],
+    });
+    res.json(events);
   })
-  res.json(events);
-}))
+);
 
 // GET / ---> Get one host's events
 router.get(
-  '/user/:userId',
+  "/user/:userId",
   asyncHandler(async (req, res, next) => {
     const { userId } = req.params;
     const { limit } = req.body;
     const events = await Event.findAll({
       where: {
-        hostId: userId
+        hostId: userId,
       },
       include: [
         { model: User, attributes: ["username"] },
         { model: Location, attributes: ["name", "locale"] },
         { model: Group, attributes: ["name"] },
       ],
-      order: [
-        ['date', 'DESC']
-      ],
+      order: [["date", "DESC"]],
       limit,
-    })
+    });
     res.json(events);
-  }))
+  })
+);
 
 // POST /   ---> Create new Event
 router.post(
@@ -99,24 +98,83 @@ router.post(
   validateEvent,
   requireAuth,
   asyncHandler(async (req, res, next) => {
-    const newGroup = await Group.create(req.body);
-    return res.json(newGroup);
+    const newEvent = await Event.create(req.body);
+    return res.json(newEvent);
   })
 );
-// // PUT /   ---> Update Event
-// router.put(
-//   "/:id",
-//   validateGroup,
-//   requireAuth,
-//   asyncHandler(async (req, res) => {
-//     const { id } = req.params;
-//     const editGroup = await Group.update(req.body, {
-//       where: {
-//         id: id,
-//       },
-//     });
-//     return res.json(editGroup);
-//   })
-// );
 
-  module.exports = router;
+// PUT /   ---> Update Event
+router.put(
+  "/:id",
+  validateEvent,
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const editEvent = await Event.update(req.body, {
+      where: {
+        id,
+      },
+    });
+    return res.json(editEvent);
+  })
+);
+
+// DELETE
+router.delete(
+  "/:eventId",
+  requireAuth,
+  asyncHandler(async (req, res, next) => {
+    const { eventId } = req.params;
+    const currentEvent = await Event.findByPk(eventId);
+    if (!currentEvent) throw new Error("Cannot find matching event");
+
+    const { token } = req.cookies;
+    await jwt.verify(token, secret, null, async (err, jwtPayload) => {
+      const { id } = jwtPayload.data;
+      const host = currentEvent.hostId === id ? true : false;
+      if (host) {
+        await currentEvent.destroy();
+      }
+    });
+    return res.json({ eventId });
+  })
+);
+
+/////// RSVP routes
+
+// POST /   ---> RSVP to an event
+router.post(
+  "/:eventId/user/:userId/add",
+  requireAuth,
+  asyncHandler(async (req, res, next) => {
+    const { eventId, userId } = req.params;
+    const newRSVP = await RSVP.create({
+      userId,
+      eventId,
+    });
+    return res.json(newRSVP);
+  })
+);
+
+// DELETE /   ---> Remove a user from a group
+router.delete(
+  "/:groupId/user/:userId/leave",
+  requireAuth,
+  asyncHandler(async (req, res, next) => {
+    const { id } = req.user;
+    const { groupId, userId } = req.params;
+    const currentUserGroup = await UserGroup.findOne({
+      where: {
+        userId,
+        groupId,
+      },
+    });
+
+    if (id === userId) {
+      await currentUserGroup.destroy();
+    }
+    return res.json({ currentUserGroup });
+  })
+);
+
+module.exports = router;
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,7 +1,7 @@
 // backend/utils/auth.js
-const jwt = require('jsonwebtoken');
-const { jwtConfig } = require('../config');
-const { User } = require('../db/models');
+const jwt = require("jsonwebtoken");
+const { jwtConfig } = require("../config");
+const { User } = require("../db/models");
 
 const { secret, expiresIn } = jwtConfig;
 
@@ -11,13 +11,13 @@ const setTokenCookie = (res, user) => {
   const token = jwt.sign(
     { data: user.toSafeObject() },
     secret,
-    { expiresIn: parseInt(expiresIn) }, // 604,800 seconds = 1 week
+    { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
   );
 
   const isProduction = process.env.NODE_ENV === "production";
 
   // Set the token cookie
-  res.cookie('token', token, {
+  res.cookie("token", token, {
     maxAge: expiresIn * 1000, // maxAge in milliseconds
     httpOnly: true,
     secure: isProduction,
@@ -38,13 +38,13 @@ const restoreUser = (req, res, next) => {
 
     try {
       const { id } = jwtPayload.data;
-      req.user = await User.scope('currentUser').findByPk(id);
+      req.user = await User.scope("currentUser").findByPk(id);
     } catch (e) {
-      res.clearCookie('token');
+      res.clearCookie("token");
       return next();
     }
 
-    if (!req.user) res.clearCookie('token');
+    if (!req.user) res.clearCookie("token");
 
     return next();
   });
@@ -55,13 +55,12 @@ const requireAuth = [
   function (req, res, next) {
     if (req.user) return next();
 
-    const err = new Error('Unauthorized');
-    err.title = 'Unauthorized';
-    err.errors = ['Unauthorized'];
+    const err = new Error("Unauthorized");
+    err.title = "Unauthorized";
+    err.errors = ["Unauthorized"];
     err.status = 401;
     return next(err);
   },
-]
-
+];
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
