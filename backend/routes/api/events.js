62326d3dee01e7211d63db6cd3975e006fdffae7
wilson <whuang3015@gmail.com>
2021-06-24 18:36:41 -0700

    add event validations and some minor styling

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,7 +1,40 @@
 const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
+const { check } = require("express-validator");
+const { requireAuth } = require("../../utils/auth");
 const { Event, User, Location, Group } = require('../../db/models');
+const { handleValidationErrors } = require("../../utils/validation");
 
+const validateEvent = [
+  check("name")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid event name."),
+  check("name")
+    .isLength({ min: 3, max: 64 })
+    .withMessage("Please provide a group name between 3 and 64 characters."),
+  check("date")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a date for this event."),
+  check("date")
+    .isDate("date", {
+      format: "MM/DD/YYYY",
+      strictMode: true,
+    })
+    .withMessage("Please provide a valid date format."),
+  check("capacity")
+    .exists({ checkFalsy: true })
+    .withMessage(
+      "Please provide a number of guests."),
+  check("capacity")
+    .isInt({ min: 0, max: 9999 })
+    .withMessage(
+      "Please provide a number of guests between 0 and 9999."),
+  check("locationId")
+    .exists({ checkFalsy: true })
+    .withMessage("Please select a location for your event."),
+  handleValidationErrors,
+];
 
 // GET / ---> Get all events
 router.get(
@@ -60,6 +93,16 @@ router.get(
     res.json(events);
   }))
 
+// POST /   ---> Create new Event
+router.post(
+  "/",
+  validateEvent,
+  requireAuth,
+  asyncHandler(async (req, res, next) => {
+    const newGroup = await Group.create(req.body);
+    return res.json(newGroup);
+  })
+);
 // // PUT /   ---> Update Event
 // router.put(
 //   "/:id",
